62e06d58c8c0ae62cd3a96c3a3d4ac4c0b59f40d
Dan4 <danyal.lakzian@homes.com.au>
2022-09-10 16:35:34 +1000

    feat: move theme switch btn to layouts
    * allowing all pages to access the theme switch btn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,39 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import '../styles/app.scss';
+
+	import { onDestroy, onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
+	import { browser } from '$app/environment';
+	import themeStore from '$lib/stores/theme';
+	import { getNextTheme, getThemeIcon, isTheme } from '$lib/utils/theme';
+	import { capitalizeWord } from '$lib/utils/typography';
+
+	let themeObserver: MutationObserver;
+
+	onMount(() => {
+		themeChange(false);
+		if (browser) {
+			themeObserver = new MutationObserver(function () {
+				if (isTheme(document?.documentElement?.dataset?.theme)) {
+					themeStore.set(document.documentElement.dataset.theme);
+				}
+			});
+			themeObserver.observe(document.documentElement, { attributes: true });
+		}
+	});
+	onDestroy(() => (themeObserver ? themeObserver.disconnect() : null));
+
+	$: themeIcon = getThemeIcon($themeStore);
+	$: nextTheme = getNextTheme($themeStore);
 </script>
 
-<slot />
+<div>
+	<!-- Pin to top right corner -->
+	<div class="absolute top-0 right-0 h-12 w-18 p-4">
+		<button class="tooltip tooltip-left" data-set-theme={nextTheme} data-act-class="ACTIVECLASS" data-tip={capitalizeWord($themeStore)}
+			>{themeIcon}</button
+		>
+	</div>
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,15 @@
 <script lang="ts">
-	import { onDestroy, onMount } from 'svelte';
-	import themeStore from '$lib/stores/theme';
-	import { themeChange } from 'theme-change';
 	import Briefcase from 'icons/Briefcase.svelte';
 	import Github from 'icons/Github.svelte';
 	import Globe from 'icons/Globe.svelte';
 	import Instagram from 'icons/Instagram.svelte';
 	import LinkedIn from 'icons/LinkedIn.svelte';
 	import Telegram from 'icons/Telegram.svelte';
-	import { getNextTheme, getThemeIcon, isTheme } from '$lib/utils/theme';
-	import { browser } from '$app/environment';
-	import { capitalizeWord } from '$lib/utils/typography';
-
-	let themeObserver: MutationObserver;
-
-	onMount(() => {
-		themeChange(false);
-		if (browser) {
-			themeObserver = new MutationObserver(function () {
-				if (isTheme(document?.documentElement?.dataset?.theme)) {
-					themeStore.set(document.documentElement.dataset.theme);
-				}
-			});
-			themeObserver.observe(document.documentElement, { attributes: true });
-		}
-	});
-	onDestroy(() => (themeObserver ? themeObserver.disconnect() : null));
-
-	$: themeIcon = getThemeIcon($themeStore);
-	$: nextTheme = getNextTheme($themeStore);
 </script>
 
 <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
 	<!--Main Col-->
-	<div
-		id="profile"
-		class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white mx-6 lg:mx-0"
-	>
+	<div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white mx-6 lg:mx-0">
 		<div class="p-4 text-center lg:text-left">
 			<!-- Image for mobile view -->
 			<div
@@ -63,29 +36,17 @@
 
 			<!-- Social links -->
 			<div class="flex flex-wrap gap-5 items-center justify-center pb-16">
-				<a
-					class="link tooltip"
-					href="https://github.com/setTimeoutInfinity"
-					data-tip="https://github.com/setTimeoutInfinity"
-				>
+				<a class="link tooltip" href="https://github.com/setTimeoutInfinity" data-tip="https://github.com/setTimeoutInfinity">
 					<Github />
 				</a>
 
 				<a class="link tooltip" href="https://t.me/talouchy" data-tip="https://t.me/talouchy">
 					<Telegram />
 				</a>
-				<a
-					class="link tooltip"
-					href="https://www.instagram.com/talouchy_art/"
-					data-tip="https://www.instagram.com/talouchy_art/"
-				>
+				<a class="link tooltip" href="https://www.instagram.com/talouchy_art/" data-tip="https://www.instagram.com/talouchy_art/">
 					<Instagram />
 				</a>
-				<a
-					class="link tooltip"
-					href="https://www.linkedin.com/in/danyallakzian"
-					data-tip="https://www.linkedin.com/in/danyallakzian"
-				>
+				<a class="link tooltip" href="https://www.linkedin.com/in/danyallakzian" data-tip="https://www.linkedin.com/in/danyallakzian">
 					<LinkedIn />
 				</a>
 			</div>
@@ -95,20 +56,6 @@
 	<!--Img Col-->
 	<div class="w-full lg:w-2/5">
 		<!-- Big profile image for side bar (desktop) -->
-		<img
-			src="/images/feel-fancy.webp"
-			class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"
-			alt="Danyal Lakzian"
-		/>
-	</div>
-
-	<!-- Pin to top right corner -->
-	<div class="absolute top-0 right-0 h-12 w-18 p-4">
-		<button
-			class="tooltip tooltip-left"
-			data-set-theme={nextTheme}
-			data-act-class="ACTIVECLASS"
-			data-tip={capitalizeWord($themeStore)}>{themeIcon}</button
-		>
+		<img src="/images/feel-fancy.webp" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block" alt="Danyal Lakzian" />
 	</div>
 </div>
