2b71003e22e83795a8a1e4274bd2bd697b0fb454
Dan4 <danyal.lakzian@homes.com.au>
2024-03-08 21:44:31 +1100

    feat: include my fav spotify playlist

--- src/routes/my-playlist/+page.svelte
+++ src/routes/my-playlist/+page.svelte
@@ -1,1 +1,33 @@
-<div>My playlist</div>
+<div class="flex flex-col gap-2 lg:gap-3 items-center justify-center h-auto lg:h-screen p-2">
+	<div class="card lg:w-1/2 w-full bg-secondary-content shadow-xl">
+		<div class="card-body">
+			<iframe
+				style="border-radius:12px"
+				src="https://open.spotify.com/embed/playlist/07GXhUV7njeqP9PqXwl8bL?utm_source=generator"
+				width="100%"
+				height="352"
+				title="My Metal playlist"
+				frameBorder="0"
+				allowfullscreen={undefined}
+				allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+				loading="lazy"
+			/>
+		</div>
+	</div>
+
+	<div class="card lg:w-1/2 w-full bg-secondary-content shadow-xl">
+		<div class="card-body">
+			<iframe
+				style="border-radius:12px"
+				src="https://open.spotify.com/embed/playlist/6VshTqI7WzQknWiCEmr7Wh?utm_source=generator"
+				width="100%"
+				height="352"
+				frameBorder="0"
+				title="My HipHop playlist"
+				allowfullscreen={undefined}
+				allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+				loading="lazy"
+			/>
+		</div>
+	</div>
+</div>
