c3d572bb3c77d6206a136654ae7d57eca99a4a29
Dan4 <danyal.lakzian@homes.com.au>
2024-03-08 21:23:34 +1100

    feat: add 'My Playlist' page navigation

--- src/lib/constants/navigation.ts
+++ src/lib/constants/navigation.ts
@@ -1,1 +1,4 @@
-export const NAVIGATION_ITEMS = [{ label: 'Home', title: 'Home', path: '/' }];
+export const NAVIGATION_ITEMS = [
+	{ label: 'Home', title: 'Home', path: '/' },
+	{ label: 'My Playlist', title: 'My Playlist', path: '/my-playlist' }
+];
